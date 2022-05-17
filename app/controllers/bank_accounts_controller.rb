e7577197a0596879bf04f7e6e3ecdfef15a827ef
Anwar Jamis <anwarjamis@gmail.com>
2022-05-17 11:18:46 -0400

    bank and categories controllers

---
+++ app/controllers/bank_accounts_controller.rb
@@ -0,0 +1,44 @@
+class BankAccountsController < ApplicationController
+  before_action :set_bank, only: %i[show edit update destroy]
+
+  def index
+    @bank_accounts = BankAccount.all
+  end
+
+  def create
+    @bank = BankAccount.new
+    # Usar la API
+    if @bank.save
+      redirect_to bank_accounts_path
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @bank = BankAccount.new(bank_params)
+    if @bank.update
+      redirect_to bank_accounts_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @bank.destroy
+  end
+
+  private
+
+  def bank_params
+    params.require(:ban).permit(:name)
+  end
+
+  def set_bank
+    @bank = BankAccount.find(params[:id])
+  end
+end
