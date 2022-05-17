f86744362890804facbd6748bded98430e04dcf4
Sebastian Jara <s.jaranilsson@gmail.com>
2022-05-17 14:36:27 -0400

    controlador de bank account actualizado con form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[show edit update]
-  resources :bank_accounts, only: %i[index show edit update destroy]
+  resources :bank_accounts, only: %i[index new show edit update destroy]
   resources :categories, except: %i[show]
   resources :movements, only: %i[index]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_214103) do
+ActiveRecord::Schema.define(version: 2022_05_17_154804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2022_05_16_214103) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "pk"
+    t.string "sk"
     t.index ["user_id"], name: "index_bank_accounts_on_user_id"
   end
 
