fd0da341792127d3240cc30f86b9e929ce79f8b9
Anwar Jamis <anwarjamis@gmail.com>
2022-05-17 16:00:39 -0400

    api is working

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -2,6 +2,6 @@ class BankAccount < ApplicationRecord
   belongs_to :user
   has_many :movements
 
-  validates :bank, :pk, :sk, :link, presence: true
+  validates :pk, :sk, :link, presence: true
   validates :name, uniqueness: true
 end
