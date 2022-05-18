93a47ecb5c2a290d42b756c0ba6662153a5ede73
Sebastian Jara <s.jaranilsson@gmail.com>
2022-05-18 14:38:14 -0400

    listo la tabla

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,5 +1,13 @@
+require "json"
+require "open-uri"
+
 class MovementsController < ApplicationController
+
   def index
-    @movements = Movement.all
+    @bank = BankAccount.where(user_id: current_user.id).first
+    url = "https://api.fintoc.com/v1/accounts/#{@bank.fintoc_id}/movements?link_token=#{@bank.link}"
+    user_serialized = URI.open(url, "Authorization" => @bank.sk).read
+    @movements = JSON.parse(user_serialized)
   end
+
 end
