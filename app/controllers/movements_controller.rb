0645fabf498908e148eb4d14c1ea4bd58bdff220
Sebastian Jara <s.jaranilsson@gmail.com>
2022-05-18 15:51:46 -0400

    ahora si q listo movements

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -4,10 +4,18 @@ require "open-uri"
 class MovementsController < ApplicationController
 
   def index
-    @bank = BankAccount.where(user_id: current_user.id).first
+    @banks = BankAccount.where(user_id: current_user.id)
+    @movements = @banks.map do |bank|
+      url = "https://api.fintoc.com/v1/accounts/#{bank.fintoc_id}/movements?link_token=#{bank.link}"
+      user_serialized = URI.open(url, "Authorization" => bank.sk).read
+      JSON.parse(user_serialized)
+    end
+  end
+
+  def bank_movements
+    @bank = BankAccount.find(params[:bank_account_id])
     url = "https://api.fintoc.com/v1/accounts/#{@bank.fintoc_id}/movements?link_token=#{@bank.link}"
     user_serialized = URI.open(url, "Authorization" => @bank.sk).read
     @movements = JSON.parse(user_serialized)
   end
-
 end
