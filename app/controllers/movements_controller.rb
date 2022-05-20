ff27a53e8cdba7250067f00c14bf5d3a1c6cb122
Sebastian Jara <s.jaranilsson@gmail.com>
2022-05-20 14:27:45 -0400

    ahora graba bien

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -9,7 +9,12 @@ class MovementsController < ApplicationController
     @banks.map do |bank|
       url = "https://api.fintoc.com/v1/accounts/#{bank.fintoc_id}/movements?link_token=#{bank.link}"
       user_serialized = URI.open(url, "Authorization" => bank.sk).read
-      JSON.parse(user_serialized)
+      movements = JSON.parse(user_serialized)
+      movements.each do |movement|
+        movement = Movement.new(category_id: 1, bank_account_id: bank.id, fintoc_id: movement['id'], amount: movement['amount'],
+                                 description: movement['description'], currency: movement['currency'], post_date: movement['post_date'])
+        movement.save
+      end
     end
     @movements = Movement.all.order(post_date: :desc)
   end
