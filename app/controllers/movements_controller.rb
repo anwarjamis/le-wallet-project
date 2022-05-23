5e253f1e75fd252f523e74e6ba5aec36cf278540
Sebastian Jara <s.jaranilsson@gmail.com>
2022-05-23 14:16:09 -0400

    listo

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -11,35 +11,35 @@ class MovementsController < ApplicationController
       user_serialized = URI.open(url, "Authorization" => bank.sk).read
       movements = JSON.parse(user_serialized)
       movements.each do |movement|
-        movement = Movement.new(category_id: 1, bank_account_id: bank.id, fintoc_id: movement['id'], amount: movement['amount'],
+        movement = Movement.new(user_id: current_user.id, category_id: 1, bank_account_id: bank.id, fintoc_id: movement['id'], amount: movement['amount'],
                                  description: movement['description'], currency: movement['currency'], post_date: movement['post_date'])
         movement.save
       end
     end
-    @movements = Movement.all.order(post_date: :desc)
+    @movements = Movement.where(user_id: current_user.id).order(post_date: :desc)
     @categories = Category.all
     if params[:bank].present? && params[:bank] != 'Todos'
       bank = BankAccount.find_by(bank: params[:bank])
-      @movements = Movement.where(bank_account_id: bank.id)
+      @movements = Movement.where(bank_account_id: bank.id, user_id: current_user.id)
     end
     if params[:bank].present? && params[:bank] == 'Todos'
-      @movements = Movement.all.order(post_date: :desc)
+      @movements = Movement.where(user_id: current_user.id).order(post_date: :desc)
     end
     if params[:category].present? && params[:category] != 'Todas'
       category = Category.find_by(name: params[:category])
-      @movements = Movement.where(category_id: category.id)
+      @movements = Movement.where(category_id: category.id, user_id: current_user.id)
     end
     if params[:category].present? && params[:category] == 'Todas'
-      @movements = Movement.all.order(post_date: :desc)
+      @movements = Movement.where(user_id: current_user.id).order(post_date: :desc)
     end
     if params[:quantity].present? && params[:quantity] != 'Todos'
       @movements = @movements.limit(params[:quantity])
     end
     if params[:category].present? && params[:category] == 'Todas'
-      @movements = Movement.all.order(post_date: :desc)
+      @movements = Movement.where(user_id: current_user.id).order(post_date: :desc)
     end
     if params[:query].present?
-      @movements = Movement.search_by_description(params[:query])
+      @movements = Movement.where(user_id: current_user.id).search_by_description(params[:query])
     end
   end
 
@@ -49,12 +49,12 @@ class MovementsController < ApplicationController
     user_serialized = URI.open(url, "Authorization" => @bank.sk).read
     @movements = JSON.parse(user_serialized)
     @movements.each do |movement|
-      @movement = Movement.new(category_id: 1, bank_account_id: @bank.id, fintoc_id: movement['id'], amount: movement['amount'],
+      @movement = Movement.new(user_id: current_user.id, category_id: 1, bank_account_id: @bank.id, fintoc_id: movement['id'], amount: movement['amount'],
                                description: movement['description'], currency: movement['currency'], post_date: movement['post_date'])
       @movement.save
     end
     if params[:query].present?
-      @movements = Movement.search_by_description(params[:query])
+      @movements = Movement.where(user_id: current_user.id).search_by_description(params[:query])
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_20_180423) do
+ActiveRecord::Schema.define(version: 2022_05_23_161903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,8 +47,10 @@ ActiveRecord::Schema.define(version: 2022_05_20_180423) do
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["bank_account_id"], name: "index_movements_on_bank_account_id"
     t.index ["category_id"], name: "index_movements_on_category_id"
+    t.index ["user_id"], name: "index_movements_on_user_id"
   end
 
   create_table "user_categories", force: :cascade do |t|
@@ -77,6 +79,7 @@ ActiveRecord::Schema.define(version: 2022_05_20_180423) do
   add_foreign_key "bank_accounts", "users"
   add_foreign_key "movements", "bank_accounts"
   add_foreign_key "movements", "categories"
+  add_foreign_key "movements", "users"
   add_foreign_key "user_categories", "categories"
   add_foreign_key "user_categories", "users"
 end
