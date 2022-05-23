cf30046b573a82d57d9a4e37dbfddb8ee4ef2654
Sebastian Jara <s.jaranilsson@gmail.com>
2022-05-23 15:59:18 -0400

    base de dato mejorada

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -13,6 +13,16 @@ class BankAccountsController < ApplicationController
       user = JSON.parse(user_serialized)
       user['balance']['available']
     end
+    @banks.each do |bank|
+      url = "https://api.fintoc.com/v1/accounts/#{bank.fintoc_id}/movements?link_token=#{bank.link}"
+      user_serialized = URI.open(url, "Authorization" => bank.sk).read
+      movements = JSON.parse(user_serialized)
+      movements.each do |movement|
+        movement = Movement.new(user_id: current_user.id, category_id: 1, bank_account_id: bank.id, fintoc_id: movement['id'], amount: movement['amount'],
+                                 description: movement['description'], currency: movement['currency'], post_date: movement['post_date'])
+        movement.save
+      end
+    end
   end
 
   def new
@@ -29,6 +39,14 @@ class BankAccountsController < ApplicationController
     @bank.account_type = user.first['official_name']
     @bank.fintoc_id = user.first['id']
     if @bank.save
+      # url = "https://api.fintoc.com/v1/accounts/#{@bank.fintoc_id}/movements?link_token=#{@bank.link}"
+      # user_serialized = URI.open(url, "Authorization" => @bank.sk).read
+      # movements = JSON.parse(user_serialized)
+      # movements.each do |movement|
+      #   movement = Movement.new(user_id: current_user.id, category_id: 1, bank_account_id: @bank.id, fintoc_id: movement['id'], amount: movement['amount'],
+      #                            description: movement['description'], currency: movement['currency'], post_date: movement['post_date'])
+      #   movement.save
+      # end
       redirect_to root_path
     else
       render 'new'
